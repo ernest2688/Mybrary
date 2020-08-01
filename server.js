27b8260441dca8bf0893cd938fdc4585f2fa0015
ernset2688 <ernest2688@gmail.com>
2020-08-01 20:29:29 +0800

    Initial Author Routes

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const express = require('express')
 const app = express()
 //import express-ejs-layouts
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
 
@@ -23,6 +25,7 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 //tell where are the public files are
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb'  ,extended : false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -33,6 +36,7 @@ db.once('open',() => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT||3000)
